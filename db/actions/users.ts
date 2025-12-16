368022924836400f2e634c8bc288f9e4f39c8b28
Akram Imtiaz <akram.imtiaz10@gmail.com>
2025-12-16 11:53:51 +0800

    update user actions

--- db/actions/users.ts
+++ db/actions/users.ts
@@ -1,28 +1,32 @@
 "use server";
 
-import { eq, not } from "drizzle-orm";
-import { revalidatePath } from "next/cache";
+import { eq } from "drizzle-orm";
+// import { revalidatePath } from "next/cache";
 import { db } from "@/db/drizzle";
-import { users } from "@/db/schema";
+import { users, type User } from "@/db/schema";
 
 export async function getUserById(id: string) {
-  const user = await db.select().from(users).where(eq(users.id, id));
+  const user = await db.query.users.findFirst({
+    where: eq(users.id, id),
+  });
 
   return user;
-};
+}
 
 export async function getUserByEmail(email: string) {
-  const user = await db.select().from(users).where(eq(users.email, email));
+  const user = await db.query.users.findFirst({
+    where: eq(users.email, email),
+  });
 
   return user;
-};
+}
 
 export async function createUser() {
-    // do something
-    return null;
+  // do something
+  return null;
 }
 
 export async function updateUser() {
-    // do something
-    return null;
+  // do something
+  return null;
 }
