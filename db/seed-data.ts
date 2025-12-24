5299258a770c0b2b3b2a622a4bbc7f9afb466269
Akram Imtiaz <akram.imtiaz10@gmail.com>
2025-12-24 09:01:23 +0800

    add seed data file

---
+++ db/seed-data.ts
@@ -0,0 +1,74 @@
+// import day from 'dayjs';
+// import weekday from 'dayjs/plugin/weekday';
+
+// day.extend(weekday);
+
+// for each class create 3 sessions 
+// and for the sessions in the past mark them as completed
+// by the teacher for that class
+// for the completed sessions - create student attendance records
+
+export const seedData = {
+    admins: [
+        {
+            name: 'Imtiyas Idris',
+            email: 'imtiaz@alma.org',
+            role: 'admin',
+            phoneNumber: '0400 000 00',
+        }
+    ],
+    teachers: [
+        {
+            name: 'Yusuf Khan',
+            email: 'yusuf@alma.org',
+            role: 'teacher',
+            phoneNumber: '0400 000 00',
+            classes: [
+                {
+                    className: 'Beginner Quran',
+                    students: [
+                        'Aisha Zaid',
+                        'Fatima Ali',
+                        'Omar Hussein',
+                        'Khalid Hassan' 
+                    ],
+                },
+                {
+                    className: 'Intermediate Tajweed',
+                    students: [
+                        'Zainab Ahmad',
+                        'Maryam Siddiqui',
+                        'Yaqoub Hashimi',
+                        'Haikal Arifie',
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Ahmed Ali',
+            email: 'ahmed@alma.org',
+            role: 'teacher',
+            phoneNumber: '0400 000 00',
+            classes: [
+                {
+                    className: 'Advanced Memorization',
+                    students: [
+                        'Mohammed Ibrahim',
+                        'Zaid Shakir',
+                        'Karim Zaid',
+                        'Farid Bahrani',
+                    ]
+                },
+                {
+                    className: 'Arabic Language',
+                    students: [
+                        'Suwaida Khalid',
+                        'Rawdah Haneeah',
+                        'Safwan Mubarak',
+                        'Tariq Ramadan',
+                    ]
+                }
+            ]
+        },
+    ]
+}
